a55274a6bed799bba3ce6f12a3d7888036c0b98b
vosilva <victorolimpio@id.uff.br>
2019-05-30 00:42:30 -0300

    Corrige autenticacao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :ensure_json_request
+  before_action :ensure_json_request, :configure_permitted_parameters, if: :devise_controller?
 
   def ensure_json_request
     return if request.headers["Accept"] =~ /json/
     render :nothing => true, :status => 406
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :email, :telefone, :tipo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :telefone, :tipo])
   end
 end
